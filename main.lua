c0f8a3c60b10195185ffea5f3cc7b6b17c2ea548
donyaSmile <32065447+donyaSmile@users.noreply.github.com>
2017-09-20 09:46:49 -0400

    day2

--- main.lua
+++ main.lua
@@ -4,4 +4,7 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+--
+local background = display.newImageRect("background.png", 360, 570)
+background.x = display.contentCenterX
+background.y = display.contentCenterY
